afa1167ea22cae9efbdeca50a3eac0d84d1bda3d
peeweesusername <92876718+peeweesusername@users.noreply.github.com>
2022-11-28 12:51:54 -0500

    adjusted goal width

--- lib/globals.dart
+++ lib/globals.dart
@@ -2,7 +2,7 @@ library airhockey.globals;
 
 const double myZoomFactor = 15;
 
-const double goalWidth = 6/myZoomFactor;
+const double goalWidth = 9/myZoomFactor;
 const double goalDepth = 0.3/myZoomFactor;
 const double goalSetBack = 1.0/myZoomFactor;
 
